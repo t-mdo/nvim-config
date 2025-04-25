89da57bf9b9366bc016fadb7fcb3d33839cdf691
Thibault Miranda de Oliveira <thibault.mirandadeoliveira@vercel.com>
2025-04-25 15:43:02 -0700

    search case improvement

--- init.lua
+++ init.lua
@@ -3,6 +3,8 @@ vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.autoindent = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
 vim.opt.formatoptions:append('cronm')
 
 vim.opt.number = true
